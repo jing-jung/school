2e99dc0b011b0f5a30818b2168100154a1bbfb51
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:29:24 +0900

    Update Assign_1.3.c

--- C언어/Assign #01 (과제명 : 수식과 연산자)/Assign_1.3.c
+++ C언어/Assign #01 (과제명 : 수식과 연산자)/Assign_1.3.c
@@ -1,1 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+  double f,c;
+  printf("화씨온도를 입력하세요.\r\n");
+  
+  scanf("%lf", &f);
+  c = (f - 32) * 5 / 9;
+  printf("섭시온도는 %f도입니다", c);
+}
