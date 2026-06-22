69678f289fc1e83b0d31edd31c1aedd51eeaf4ff
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:32:07 +0900

    Update Assign_1.6.c

--- C언어/Assign #01 (과제명 : 수식과 연산자)/Assign_1.6.c
+++ C언어/Assign #01 (과제명 : 수식과 연산자)/Assign_1.6.c
@@ -1,1 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+  double weight, height, bmi;
+  printf("키 입력(단위 m) : ");
+  scanf_s("%lf", &height);
+
+  printf("몸 무게 입력(단위 kg) : ");
+  scanf_s("%lf", &weight);
+
+  bmi = weight / height / height;
+  printf("***************\nBMI 지수 : %.1lf", bmi);
+}
