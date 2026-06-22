430f2721311ae1b5c250ef78c420893624aea348
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:52:38 +0900

    Update Assign_4.3.c

--- C언어/Assign #04 (과제명 : 선택문)/Assign_4.3.c
+++ C언어/Assign #04 (과제명 : 선택문)/Assign_4.3.c
@@ -1,1 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+
+  int m, s;
+  char a;
+
+  printf("등급(A~D)과 무게(gram)을 공백을 넣어 입력하시오 : ");
+  scanf("%c %d", &a, &m);
+
+  switch (a)
+  {
+    case 'A': s = m / 100 * 12000; break;
+    case 'B': s = m / 100 * 10000; break;
+    case 'C': s = m / 100 * 7000; break;
+    case 'D': s = m / 100 * 5000; break;
+    default:
+      break;
+}
+
+  printf("%c등급 %d그램의 가격은 %d원입니다.", a, m, s);
+  return 0;
+}
