470799410417fab96298e8e0a0b661993a91fcff
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:46:10 +0900

    Update Assig_3.2.c

--- C언어/Assign #03 (과제명 : 수식과 연산자)/Assig_3.2.c
+++ C언어/Assign #03 (과제명 : 수식과 연산자)/Assig_3.2.c
@@ -1,1 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+  float x, b;
+
+  printf("x의 값을 입력하세요 : ");
+  scanf("%f", &x);
+
+  b = (2 * x * x * x) - (6 * x) + 1 / (x + 1);
+  printf("f(%.3f)=%.3f", x, b);
+  
+  return 0;
+}
