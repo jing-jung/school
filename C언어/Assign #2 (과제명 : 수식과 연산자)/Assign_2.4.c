3669f25ca88b5a7c4990a8af61ec958e2bc42bb2
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:38:58 +0900

    Update Assign_2.4.c

--- C언어/Assign #2 (과제명 : 수식과 연산자)/Assign_2.4.c
+++ C언어/Assign #2 (과제명 : 수식과 연산자)/Assign_2.4.c
@@ -1,1 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+  float fuel = 60;
+  float cost = 1995;
+  float Fuel_efficiency = 17.8;
+  float distance = fuel * Fuel_efficiency;
+  float price = cost * 60;
+
+  printf("총 주유비:%2If", price);
+  printf("주행 가능한 거리: %2If", distance);
+  return 0;
+}
