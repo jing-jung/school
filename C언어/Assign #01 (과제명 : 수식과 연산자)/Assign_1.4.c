3260ee02796b9e2911a0e6e8786b285d5391c044
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:30:03 +0900

    Update Assign_1.4.c

--- C언어/Assign #01 (과제명 : 수식과 연산자)/Assign_1.4.c
+++ C언어/Assign #01 (과제명 : 수식과 연산자)/Assign_1.4.c
@@ -1,1 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int Multiply(int a, int b)
+{
+  return a * b;
+}
+
+int main()
+{
+  int length, height, width;
+  
+  printf("상자의 길이, 높이, 깊이를 순서대로 입력하세요.\r\n");
+  scanf("%d%d%d", &width, &length, &height);
+
+  int volume = Multiply(Multiply(height, length), width);
+  printf("상자의 부피는 %d입니다.", volume);
+}
