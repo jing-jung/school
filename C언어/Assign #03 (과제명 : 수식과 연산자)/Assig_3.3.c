fe58f5cab838be17a29b4aeaebdba164ed660524
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:46:39 +0900

    Update Assig_3.3.c

--- C언어/Assign #03 (과제명 : 수식과 연산자)/Assig_3.3.c
+++ C언어/Assign #03 (과제명 : 수식과 연산자)/Assig_3.3.c
@@ -1,1 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+  float cm, ft, in;
+
+  printf("키를 입력하시요(cm) : ");
+  scanf("%f", &cm);
+
+  printf("***************\r\n");
+
+  in = cm/2.54;
+  ft = in/12;
+
+  printf("%.0fcm은 %.2f피트 %.2f인치입니다", cm, ft, in);
+  return 0;
+}
