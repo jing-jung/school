88662b754bac0d484af2516b92b655b13884643c
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:47:29 +0900

    Update Assig_3.4.c

--- C언어/Assign #03 (과제명 : 수식과 연산자)/Assig_3.4.c
+++ C언어/Assign #03 (과제명 : 수식과 연산자)/Assig_3.4.c
@@ -1,1 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+  float x,y;
+
+  printf("x 좌표를 입력하시오 : ");
+  scanf("%f", &x);
+
+  printf("y 좌표를 입력하시오 : ");
+  scanf("%f", &y);
+
+  printf("***************\r\n");
+
+  (x > 0 && y > 0) ? printf("(%0.f,%0.f)은 1사분면에 위치",x,y) : printf(" ");
+  (x < 0 && y > 0) ? printf("(%0.f,%0.f)은 2사분면에 위치",x,y) : printf(" ");
+  (x < 0 && y < 0) ? printf("(%0.f,%0.f)은 3사분면에 위치",x,y) : printf(" ");
+  (x > 0 && y < 0) ? printf("(%0.f,%0.f)은 4사분면에 위치",x,y) : printf(" ");
+
+  return 0;
+}
