9bfbb56fde5d6f6ab61124f98417801eec82a42a
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:30:39 +0900

    Update Assign_1.5.c

--- C언어/Assign #01 (과제명 : 수식과 연산자)/Assign_1.5.c
+++ C언어/Assign #01 (과제명 : 수식과 연산자)/Assign_1.5.c
@@ -1,1 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+  const double PI = 3.14159;
+  double r;
+  printf("반지름을 입력하세요.\r\n");
+
+  scanf("%lf", &r);
+  double volume = 4 * PI * r * r * r / 3;
+  double area = PI * 4 * r * r;
+
+  printf("****************\n\r구의 부피: %f\n\r구의 겉넓이: %f", volume, area);
+}
