ff08f459963e83822c41094cefa91db73be1b14b
jing-jung <yhn5729@skuniv.ac.kr>
2026-06-22 11:53:21 +0900

    Update Assign_4.4.c

--- C언어/Assign #04 (과제명 : 선택문)/Assign_4.4.c
+++ C언어/Assign #04 (과제명 : 선택문)/Assign_4.4.c
@@ -1,1 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main() {
+
+  char a;
+
+  printf("문자를 입력하시오 : ");
+  scanf("%c", &a);
+
+  printf("***************\n");
+
+  if (a == 65 || a == 69 || a == 73 || a == 79 || a == 85)
+    printf("입력한 문자 %c는 모음입니다.", a);
+  else if (a == 97 || a == 101 || a == 105 || a == 111 || a == 117)
+    printf("입력한 문자 %c는 모음입니다.", a);
+  else
+    printf("입력한 문자 %c는 자음입니다.", a);
+  
+  return 0;
+}
